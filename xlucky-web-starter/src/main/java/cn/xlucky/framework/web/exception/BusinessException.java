ed963ac1abdb6b84df1754b5bac7055d5773459c
聂祥 <1006556989@qq.com>
2021-06-04 11:27:17 +0800

    配置异常

--- xlucky-web-starter/src/main/java/cn/xlucky/framework/web/exception/BusinessException.java
+++ xlucky-web-starter/src/main/java/cn/xlucky/framework/web/exception/BusinessException.java
@@ -1,7 +1,7 @@
 package cn.xlucky.framework.web.exception;
 
+import cn.xlucky.framework.common.config.ExceptionCodeConfig;
 import cn.xlucky.framework.common.dto.enums.ResultCodeEnum;
-import cn.xlucky.framework.web.config.ExceptionCodeConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
